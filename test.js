0290f6edb0f39a75a649837980acbd8cc1ce3344
AllenW1991 <venecia631201@yahoo.com.tw>
2021-10-14 13:08:43 +0800

    test

--- test.js
+++ test.js
@@ -7,11 +7,11 @@ window.onload = function () {
     var outer = document.getElementById("slide_show_outer");
     nav_dots.style.left = (outer.offsetWidth - nav_dots.offsetWidth) / 2 + "px";
 
-    var ad_index = 0;
+    // var ad_index = 0;
     var ads = document.getElementsByTagName("a");
     var next = document.getElementById("next");
     var prev = document.getElementById("prev");
-
+    next_prev()
 
     for (i = 0; i < ads.length; i++) {
         ads[i].num = i;
@@ -28,13 +28,43 @@ window.onload = function () {
         }
         ads[index].style.backgroundColor = "rgba(223, 223, 223, 0.8)"
     }
-    function next_prev() {
-        next.onclick = function () {
-            
 
+    function next_prev() {
+        var x = 0;
+        ads[x].style.backgroundColor = "rgba(223, 223, 223, 0.8)"
+        next.onclick = function () {
+            x += 1;
+            ad_list.style.left = x * -960 + "px";
+            if (x >= ads.length) {
+                x = 0;
+                ad_list.style.left = x * -960 + "px";
+            } else if (x < 0) {
+                x = x + ads.length;
+                ad_list.style.left = x * -960 + "px";
+            }
+            for (var i = 0; i < ads.length; i++) {
+                ads[i].style.backgroundColor = "";
+            }
+            ads[x].style.backgroundColor = "rgba(223, 223, 223, 0.8)"
         }
         prev.onclick = function () {
-            ad_list.style.left -= 960 + "px";
+            x -= 1;
+            for (var i = 0; i < ads.length; i++) {
+                ads[i].style.backgroundColor = "";
+            }
+            if (x >= 0) {
+                ad_list.style.left = x * -960 + "px";
+                ads[x].style.backgroundColor = "rgba(223, 223, 223, 0.8)"
+            }
+            if (x < 0) {
+                ad_list.style.left = (x + ads.length) * -960 + "px";
+                ads[x + ads.length].style.backgroundColor = "rgba(223, 223, 223, 0.8)"
+            }
+            if (x <= -10) {
+                x = 0;
+                ad_list.style.left = x * -960 + "px";
+                ads[x].style.backgroundColor = "rgba(223, 223, 223, 0.8)"
+            }
 
         }
     }
